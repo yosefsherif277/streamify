23a253c13e30d0019c0bc54509d9f17527f37a6d
Yosef Sherif <yasherif92@gmail.com>
2025-08-03 19:00:03 +0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,14 +29,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
